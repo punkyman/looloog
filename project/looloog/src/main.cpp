c49cdc9407d70ea505f3683e2838067173f9e40e
punkyman <djbiggouji@yahoo.fr>
2026-03-20 21:58:13 +0100

    random setup of main

--- project/looloog/src/main.cpp
+++ project/looloog/src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
+#include "midihandler/midihandler.h"
 
 void setup() {
   Serial.begin(9600);
+  // midi initialization without any input expected
+  MIDI.begin(MIDI_CHANNEL_OFF);
 }
 
 void loop() {
-  delay(2000);
+// update panel values
+
+// cap refresh rate to 60hz
+  delay(16);
   Serial.println("Ping");
 }
 
