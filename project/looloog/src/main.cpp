8e47ad56f427ff2577a866a872e595228fbe78a9
punkyman <djbiggouji@yahoo.fr>
2026-03-21 08:21:41 +0100

    first throw of the main, does not work for now

--- project/looloog/src/main.cpp
+++ project/looloog/src/main.cpp
@@ -1,5 +1,15 @@
 #include <Arduino.h>
 #include "midihandler/midihandler.h"
+#include "pot/pot.h"
+#include "button/button.h"
+#include "mux/mux.h"
+
+Pot g_pot1(1, 1, 15);
+Pot g_pot2(1, 2, 7);
+Button g_button1(1, 3, 15);
+
+Mux g_mux1;
+Mux g_mux2;
 
 void setup() {
   Serial.begin(9600);
@@ -8,7 +18,10 @@ void setup() {
 }
 
 void loop() {
+  
 // update panel values
+  g_mux1.Update();
+  g_mux2.Update();
 
 // cap refresh rate to 60hz
   delay(16);
