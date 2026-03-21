1b4433e3dea262069378cc08601a3ebe00e93c4a
punkyman <djbiggouji@yahoo.fr>
2026-03-21 11:00:04 +0100

    implement the notion of address, so that we updat only the existing controls

--- project/looloog/src/control/control.h
+++ project/looloog/src/control/control.h
@@ -6,10 +6,13 @@ class Control
     protected:
         midi::DataByte m_controlNumber;
         midi::Channel m_channelNumber;
+        int m_muxAddress;
+
 
     public:
-        Control(midi::DataByte _controlNumber, midi::Channel _channelNumber, int _muxPin)
-                : m_controlNumber(_controlNumber), m_channelNumber(_channelNumber)
+        Control(midi::DataByte _controlNumber, midi::Channel _channelNumber, int _muxAddress)
+                : m_controlNumber(_controlNumber), m_channelNumber(_channelNumber), m_muxAddress(_muxAddress)
         {}
+        int GetMuxAdress() { return m_muxAddress; }
         virtual void Update(int _muxPin) = 0;
 };
--- project/looloog/src/mux/mux.cpp
+++ project/looloog/src/mux/mux.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include "mux.h"
 
-Mux::Mux(Control* _controls, int _numControls, int _s0Pin, int _s1Pin, int _s2Pin, int _s3Pin, int _cableSelectPin, int _outputPin)
-            : m_controls(_controls), m_numControls(_numControls), m_s0Pin(_s0Pin), m_s1Pin(_s1Pin), m_s2Pin(_s2Pin), m_s3Pin(_s3Pin), m_cableSelectPin(_cableSelectPin), m_outputPin(_outputPin)
+Mux::Mux(Control *_controls, int _numControls, int _s0Pin, int _s1Pin, int _s2Pin, int _s3Pin, int _cableSelectPin, int _outputPin)
+    : m_controls(_controls), m_numControls(_numControls), m_s0Pin(_s0Pin), m_s1Pin(_s1Pin), m_s2Pin(_s2Pin), m_s3Pin(_s3Pin), m_cableSelectPin(_cableSelectPin), m_outputPin(_outputPin)
 {
     pinMode(m_s0Pin, OUTPUT);
     pinMode(m_s1Pin, OUTPUT);
@@ -16,22 +16,28 @@ void Mux::Update()
 {
     Serial.println("\nupdate mux component on CS pin " + m_cableSelectPin);
 
-    for(int i = 0; i < m_numControls && i < 16; ++i)
+    for (int i = 0; i < 16; ++i)
     {
-        bool s0 = m_numControls & 0b1000;
-        bool s1 = m_numControls & 0b0100;
-        bool s2 = m_numControls & 0b0010;
-        bool s3 = m_numControls & 0b0001;
+        for (int j = 0; j < m_numControls && j < 16; ++j)
+        {
+            if (m_controls[j].GetMuxAdress() == i)
+            {
+                bool s0 = i & 0b1000;
+                bool s1 = i & 0b0100;
+                bool s2 = i & 0b0010;
+                bool s3 = i & 0b0001;
 
-        digitalWrite(m_s0Pin, s0);
-        digitalWrite(m_s0Pin, s1);
-        digitalWrite(m_s0Pin, s2);
-        digitalWrite(m_s0Pin, s3);
+                digitalWrite(m_s0Pin, s0);
+                digitalWrite(m_s0Pin, s1);
+                digitalWrite(m_s0Pin, s2);
+                digitalWrite(m_s0Pin, s3);
 
-        int debugValue = s0 * 0b1000 + s1 * 0b0100 + s2 * 0b0010 + s3 * 0b0001;
+                int debugValue = s0 * 0b1000 + s1 * 0b0100 + s2 * 0b0010 + s3 * 0b0001;
 
-        Serial.println("\nupdate for component number " + debugValue);
+                Serial.println("\nupdate for component number " + debugValue);
 
-        m_controls[i].Update(m_outputPin);
+                m_controls[i].Update(m_outputPin);
+            }
+        }
     }
 }
