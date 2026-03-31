de6ac491d9432ee4c0bd9b2cc6833e6a901a2e6f
punkyman <djbiggouji@yahoo.fr>
2026-03-31 22:40:33 +0200

    fix s0/s3 order, simplify update loop

--- project/looloog/src/mux/mux.cpp
+++ project/looloog/src/mux/mux.cpp
@@ -26,7 +26,7 @@ void Mux::InitMuxBus(int _s0Pin, int _s1Pin, int _s2Pin, int _s3Pin)
     pinMode(m_s0Pin, OUTPUT);
     pinMode(m_s1Pin, OUTPUT);
     pinMode(m_s2Pin, OUTPUT);
-    pinMode(m_s3Pin, OUTPUT);    
+    pinMode(m_s3Pin, OUTPUT);
 }
 
 // enable component
@@ -34,33 +34,29 @@ void Mux::InitMuxBus(int _s0Pin, int _s1Pin, int _s2Pin, int _s3Pin)
 // 74HC4067 component frequency change is 89mhz, so this code should be safe to execute without additional delay
 void Mux::Update()
 {
-    //Serial.println("\nupdate mux component on CS pin " + m_cableSelectPin);
+    // Serial.println("\nupdate mux component on CS pin " + m_cableSelectPin);
 
     digitalWrite(m_cableSelectPin, LOW);
 
-    for (int i = 0; i < 16; ++i)
+    for (int i = 0; i < m_numControls && i < 16; ++i)
     {
-        for (int j = 0; j < m_numControls && j < 16; ++j)
-        {
-            if (m_controls[j]->GetMuxAdress() == i)
-            {
-                bool s0 = i & 0b1000;
-                bool s1 = i & 0b0100;
-                bool s2 = i & 0b0010;
-                bool s3 = i & 0b0001;
+        int address = m_controls[i]->GetMuxAdress();
+        bool s3 = address & 0b1000;
+        bool s2 = address & 0b0100;
+        bool s1 = address & 0b0010;
+        bool s0 = address & 0b0001;
 
-                digitalWrite(m_s0Pin, s0);
-                digitalWrite(m_s0Pin, s1);
-                digitalWrite(m_s0Pin, s2);
-                digitalWrite(m_s0Pin, s3);
+        digitalWrite(m_s0Pin, s0 ? HIGH : LOW);
+        digitalWrite(m_s1Pin, s1 ? HIGH : LOW);
+        digitalWrite(m_s2Pin, s2 ? HIGH : LOW);
+        digitalWrite(m_s3Pin, s3 ? HIGH : LOW);
 
-                int debugValue = s0 * 0b1000 + s1 * 0b0100 + s2 * 0b0010 + s3 * 0b0001;
+        int debugValue = s3 * 0b1000 + s2 * 0b0100 + s1 * 0b0010 + s0 * 0b0001;
 
-                Serial.println("\nupdate for component number " + debugValue);
+        //Serial.println("\nupdate for component number ");
+        //Serial.println(debugValue);
 
-                m_controls[j]->Update(m_outputPin);
-            }
-        }
+        m_controls[i]->Update(m_outputPin);
     }
 
     digitalWrite(m_cableSelectPin, HIGH);
