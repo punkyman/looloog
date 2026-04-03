389925d619390608533eb1ef2002343c85a3f0c2
punkyman <djbiggouji@yahoo.fr>
2026-04-03 09:59:45 +0200

    configure code for 2 muxes

--- project/looloog/src/main.cpp
+++ project/looloog/src/main.cpp
@@ -13,12 +13,18 @@
 #define BUS_S2_PIN 4
 #define BUS_S3_PIN 5
 
-Control* g_muxControls[] = {
-  new Pot(0, 3, 14),
+Control* g_muxControls1[] = {
+  new Pot(0, 3, 15),
+};
+
+Control* g_muxControls2[] = {
+  new Pot(1, 3, 14),
   new Button(1, 3, 15),
 };
 
-Mux g_mux(g_muxControls, 2, D7, A2);
+Mux g_mux1(g_muxControls1, 1, D6, A3);
+
+Mux g_mux2(g_muxControls2, 2, D7, A2);
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +40,9 @@ void loop() {
   unsigned long start = millis();
 
   // update each mux to send midi packets
-  g_mux.Update();
+  g_mux1.Update();
+
+  g_mux2.Update();
 
   unsigned long duration = millis() - start;
 
