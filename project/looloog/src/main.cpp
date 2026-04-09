40b6b539e9ba92be948b0be62dbd2ac18c1c765c
punkyman <djbiggouji@yahoo.fr>
2026-04-09 10:47:02 +0200

    rewrite main to match pcb structure and layout

--- project/looloog/src/main.cpp
+++ project/looloog/src/main.cpp
@@ -4,44 +4,41 @@
 #include "pot/pot.h"
 #include "button/button.h"
 #include "mux/mux.h"
+#include "board_utils.h"
 
 #define D6 6
 #define D7 7
+#define D8 8
+#define D9 9
 
 #define BUS_S0_PIN 2
 #define BUS_S1_PIN 3
 #define BUS_S2_PIN 4
 #define BUS_S3_PIN 5
 
-Control* g_muxControls1[] = {
-  new Pot(1, 15),
-};
+#define MODULE_1_MIDI_CHANNEL 1
+#define MODULE_2_MIDI_CHANNEL 2
 
-Control* g_muxControls2[] = {
-  new Pot(2, 14),
-  new Button(2, 15),
-};
-
-Mux g_mux1(g_muxControls1, 1, D6, A3);
-
-Mux g_mux2(g_muxControls2, 2, D7, A2);
+DECLARE_MUX(g_module1Mux1, D6, A3, MODULE_1_MIDI_CHANNEL)
+DECLARE_MUX(g_module1Mux2, D7, A2, MODULE_1_MIDI_CHANNEL)
+DECLARE_MUX(g_module2Mux1, D8, A1, MODULE_2_MIDI_CHANNEL)
+DECLARE_MUX(g_module2Mux2, D9, A0, MODULE_2_MIDI_CHANNEL)
 
 void setup() {
   Serial.begin(9600);
 
   // initialize bus shared by all mux
   Mux::InitMuxBus(BUS_S0_PIN, BUS_S1_PIN, BUS_S2_PIN, BUS_S3_PIN);
-
-  
 }
 
 void loop() {
   unsigned long start = millis();
 
   // update each mux to send midi packets
-  g_mux1.Update();
-
-  g_mux2.Update();
+  g_module1Mux1.Update();
+  g_module1Mux2.Update();
+  g_module2Mux1.Update();
+  g_module2Mux2.Update();
 
   unsigned long duration = millis() - start;
 
