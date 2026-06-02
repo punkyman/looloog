d4d4f50b3ea2bbea08636215a0a3d638d9722c1e
punkyman <djbiggouji@yahoo.fr>
2026-06-02 11:39:12 +0200

    implement a jitter margin to compensate for DAC measurements precision

--- project/looloog/src/pot/pot.cpp
+++ project/looloog/src/pot/pot.cpp
@@ -1,6 +1,8 @@
 #include "pot.h"
 #include "midihandler/midihandler.h"
 
+#define JITTER_MARGIN 4
+
 Pot::Pot(midi::Channel _channelNumber, midi::DataByte _controlNumber)
         : Control(_channelNumber, _controlNumber)
         , m_previousValue(0)
@@ -11,7 +13,7 @@ void Pot::Update(int _muxPin)
 { 
     int value = analogRead(_muxPin);
 
-    if(m_previousValue != value)
+    if(value >= (m_previousValue + JITTER_MARGIN) || value <= ( m_previousValue - JITTER_MARGIN))
     {
         // as arduino DAC encodes on 10bit (0-1024), reduce range to (0-128)
         midi::DataByte midiValue = value >> 3;
